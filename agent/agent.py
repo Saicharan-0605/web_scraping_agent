db2adc65e2361e4bdaae2e47c9d8e56a87d86d31
Saicharan-0605 <saicharanreddy.jogannagari@covasant.com>
2025-07-21 10:21:12 +0530

    Update agent.py

--- agent/agent.py
+++ agent/agent.py
@@ -1,10 +1,14 @@
+import os
 from google.adk.agents import LlmAgent
 from .web_scraping_tool import scrape_website
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 test_agent = LlmAgent(
     name="web_agent",
-    model="gemini-2.5-flash",
+    model=os.environ.get("MODEL_NAME"),
     description="scrapes web for information",
     instruction="Assist user by providing information from provided webpage URL",
     tools=[scrape_website]
